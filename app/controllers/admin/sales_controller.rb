4304a83b0e40546f1bd65cfea3118ac83a41c4db
emi-hi <emily.hillier@gmail.com>
2019-11-15 18:11:16 +0000

    styled login form, put add button on product description page, styled new sale form and fixed routes

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -3,4 +3,31 @@ class Admin::SalesController < ApplicationController
     @sales = Sale.all
   end
 
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
+  end
+
+  
+  private 
+
+  def sale_params
+    params.require(:sale).permit(
+      :name,
+      :percent_off,
+      :starts_on,
+      :ends_on
+    )
+  end
+
+
 end
