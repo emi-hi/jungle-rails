f3b1db7f9037b471a42beff7ca3aa55b5b2593ba
emi-hi <emily.hillier@gmail.com>
2019-11-19 03:19:12 +0000

    added tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.where('lower(email) = lower(?)', email.strip).first
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,9 +11,10 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
     it "is not valid without a unique email address" do
-      subject.save
-      subject1 = User.new first_name: 'Test', last_name: 'Tester2', email: 'test@example.COM', password: '123', password_confirmation: '123'
-      expect(subject1.save).to be false
+      @user1 = User.new first_name: 'Test', last_name: 'Tester2', email: 'test@example.com', password: '123', password_confirmation: '123'
+      @user1.save
+      @user2 = User.new first_name: 'Test', last_name: 'Tester2', email: 'TEST@example.COM', password: '123', password_confirmation: '123'
+      expect(@user2.save).to be false
     end
     it "is not valid without an email address" do
       subject.email = nil
@@ -27,10 +28,18 @@ RSpec.describe User, type: :model do
       subject.last_name = nil
       expect(subject).to_not be_valid
     end
+    it "is not valid with a short password" do
+      subject.password = '1'
+      expect(subject).to_not be_valid
+    end
+  end
+  describe '.authenticate_with_credentials' do
+    it "should not validate with wrong credentials" do
+      @user = User.create(first_name: 'Test', last_name: "Testerson",
+      email: 'test@example.com', password: '123', password_confirmation: '123')
+      authenticate = User.authenticate_with_credentials(@user.email, @user.password)
+      expect(authenticate).to_not be_valid
+    end
   end
 
-  describe '.authenticate_with_credentials' do
-    
-    # examples for this class method here
-  end
 end
